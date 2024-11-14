49318fe74cfada89cae347653bdf71cdbcc5a45b
devalinaqvi <dev.alinaqvi@gmail.com>
2024-11-14 15:40:12 +0500

    Assign types for function and variable

--- typescript/index.ts
+++ typescript/index.ts
@@ -2,10 +2,10 @@ let num: number = 2;
 
 console.log(num);
 
-function add(num1: number, num2: number) {
+function add(num1: number, num2: number): number {
     return num1 + num2;
 }
 
-let sum = add(1,2);
+let sum: number = add(1,2);
 
 console.log(sum);
