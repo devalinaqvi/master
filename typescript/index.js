87b34141c9a558bdbbab3de21a5d7898cf0f531e
devalinaqvi <dev.alinaqvi@gmail.com>
2024-11-14 15:37:06 +0500

    Create a new add function in typescript

--- typescript/index.js
+++ typescript/index.js
@@ -1,3 +1,8 @@
 "use strict";
 let num = 2;
 console.log(num);
+function add(num1, num2) {
+    return num1 + num2;
+}
+let sum = add(1, 2);
+console.log(sum);
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,3 +1,11 @@
 let num: number = 2;
 
 console.log(num);
+
+function add(num1: number, num2: number) {
+    return num1 + num2;
+}
+
+let sum = add(1,2);
+
+console.log(sum);
